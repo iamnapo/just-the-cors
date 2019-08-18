929597d53eb8fcac839766d69574565b54018bfe
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-08-18 23:15:20 +0300

    Update README.md, Update eslint

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = (...args) => {
-  if (args.length === 1 && typeof args[0] === 'function') { // It's the handler
+  if (args.length === 1 && typeof args[0] === "function") {
     const cb = args[0];
     return (req, res) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Credentials', 'true');
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
 
-      if (req.method.toUpperCase() === 'OPTIONS') {
-        res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-        res.setHeader('Access-Control-Request-Headers', 'Vary');
-        const reqHeaders = req.headers['access-control-request-headers'];
-        if (reqHeaders && reqHeaders.length) res.setHeader('Access-Control-Allow-Headers', reqHeaders);
-        res.setHeader('Content-Length', '0');
+      if (req.method.toUpperCase() === "OPTIONS") {
+        res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+        res.setHeader("Access-Control-Request-Headers", "Vary");
+        const reqHeaders = req.headers["access-control-request-headers"];
+        if (reqHeaders && reqHeaders.length) res.setHeader("Access-Control-Allow-Headers", reqHeaders);
+        res.setHeader("Content-Length", "0");
         res.statusCode = 204;
         return res.end();
       }
@@ -19,17 +19,17 @@ module.exports = (...args) => {
     };
   }
 
-  const [req, res = { setHeader: () => {}, statusCode: null, end: () => {} }] = args;
+  const [req, res = { setHeader: () => {}, end: () => {} }] = args;
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
-  if (req.method.toUpperCase() === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.setHeader('Access-Control-Request-Headers', 'Vary');
-    const reqHeaders = req.headers['access-control-request-headers'];
-    if (reqHeaders && reqHeaders.length) res.setHeader('Access-Control-Allow-Headers', reqHeaders);
-    res.setHeader('Content-Length', '0');
+  if (req.method.toUpperCase() === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.setHeader("Access-Control-Request-Headers", "Vary");
+    const reqHeaders = req.headers["access-control-request-headers"];
+    if (reqHeaders && reqHeaders.length) res.setHeader("Access-Control-Allow-Headers", reqHeaders);
+    res.setHeader("Content-Length", "0");
     res.statusCode = 204;
     return res.end();
   }
